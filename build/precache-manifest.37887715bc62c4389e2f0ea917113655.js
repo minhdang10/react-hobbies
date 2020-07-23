6338b8027780467b3db711a60ba89de6224c51fa
minhdang1110 <nhatminhdang04@gmail.com>
2020-07-23 15:31:55 -0400

    add deployment

---
+++ build/precache-manifest.37887715bc62c4389e2f0ea917113655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40295d20b5eada7dadd3dc012e01944d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1c2c30f1bf5873400f",
+    "url": "/static/css/main.0779775a.chunk.css"
+  },
+  {
+    "revision": "aa99633144ba5b2b62b9",
+    "url": "/static/js/2.d6794966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6794966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1c2c30f1bf5873400f",
+    "url": "/static/js/main.9fcad69b.chunk.js"
+  },
+  {
+    "revision": "fb5aaa11b94fef2ddf6a",
+    "url": "/static/js/runtime-main.98cd6f16.js"
+  }
+]);
